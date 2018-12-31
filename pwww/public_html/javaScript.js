df7a95922270c5477426d86a85dca5962f48330b
Vesa Berdyna <vesaberdyna@gmail.com>
2018-12-31 01:40:47 +0100

    update

---
+++ pwww/public_html/javaScript.js
@@ -0,0 +1,57 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var actors = [
+  "Jennifer Lawrence",
+  "Johnny Depp",
+  "Angelina Jolie",
+   "Marlon Brando",
+  "Robert De Niro",
+  "Leonardo DiCaprio",
+   "Morgan Freeman",
+  "Brad Pitt",
+  "Tom Cruise"];
+  
+  
+  
+
+               function changes(){
+ var stars =["Bryan Cranston",	
+"Anna Gunn",	
+"Aaron Paul",
+"Dean Norris",
+"Betsy Brandt",	 
+"RJ Mitte",	 
+"Bob Odenkirk",
+"Steven Michael Quezada", 
+"Jonathan Banks",
+"Giancarlo Esposito",
+"Charles Baker"];
+
+document.getElementById("bBad").innerHTML = stars;
+}
+
+ function changes1(){
+ var stars =["Bryan Cranston",	
+"Anna Gunn",	
+"Aaron Paul",
+"Dean Norris",
+"Betsy Brandt",	 
+"RJ Mitte",	 
+"Bob Odenkirk",
+"Steven Michael Quezada", 
+"Jonathan Banks",
+"Giancarlo Esposito",
+"Charles Baker"];
+
+document.getElementById("gth").innerHTML = stars;
+}
+
+
+
+function msg(){  
+ alert("Hello Javatpoint");  
+}
+//console.warn('Hi!');
