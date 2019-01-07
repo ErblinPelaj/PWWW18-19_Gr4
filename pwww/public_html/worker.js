5764cbe353aafe788a06fc0dafa19e48b7515693
Vesa Berdyna <vesaberdyna@gmail.com>
2019-01-07 18:34:51 +0100

    update

---
+++ pwww/public_html/worker.js
@@ -0,0 +1,10 @@
+
+this.onmessage = e => {
+    const delay = e.data;
+
+    const start = performance.now();
+    while (performance.now() - start < delay);
+    const end = performance.now();
+
+    this.postMessage(end - start);
+};
